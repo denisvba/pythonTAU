baa2c402abb2a82464b3053b44283c28e55229c6
Denis Vitor Bueno de Almeida <denisvba@motorola.com>
2020-06-22 06:17:45 -0300

    update repo

---
+++ chapter_01/variables.py
@@ -0,0 +1,37 @@
+# python print() statement
+# short comment, explaning small bits of code
+# usually, function and variable declaration or usage
+
+_cars = 23
+cars = 24
+
+SOLD_CARS = 35
+SOLD_CARS = 3555
+number_of_cars = 23
+kind_of_cars = "Ferrari"
+
+print(cars)
+print(_cars)
+print(kind_of_cars)
+print(SOLD_CARS)
+print(number_of_cars)
+
+
+"""
+This is a multi-line comment
+YOu can use this kind of comment to 
+make longer noters as you are learning.
+In Python, these are often used as 
+docstrigs.
+
+We will do formatting now.
+"""
+
+name = "Janelle Jones"
+type_of_car = "Rolls Royce"
+school = "Foundation elementary school"
+
+print(name + " works at" + school)
+
+# python string.format()
+print("{} works at {} .".format(name, school))
